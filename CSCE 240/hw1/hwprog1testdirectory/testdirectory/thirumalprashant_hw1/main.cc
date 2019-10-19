ccd6032e433c55595cfa90cb8b70a71555d3ad2c
Prashant <51274399+PrashantThirumal@users.noreply.github.com>
2019-10-18 23:41:32 -0400

    Add files via upload

---
+++ CSCE 240/hw1/hwprog1testdirectory/testdirectory/thirumalprashant_hw1/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/****************************************************************
+ * Main program for Homework 1.
+ *
+ * Author/copyright:  Prashant Thirumal. All rights reserved.
+ * Date: 3 September 2018
+**/
+
+int main(int argc, char *argv[]) {
+  cout << "Hello, world." << endl;
+  cout << "My name is Prashant Thirumal." << endl;
+
+  return 0;
+}
