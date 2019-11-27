8a4ae3895084f9857c957a1f20a6491ab39d7ecf
Prashant <51274399+PrashantThirumal@users.noreply.github.com>
2019-11-26 20:23:34 -0500

    Update kernel_readAddr.c

--- CSCE 311/Project 3/kernel_readAddr.c
+++ CSCE 311/Project 3/kernel_readAddr.c
@@ -7,30 +7,30 @@
 
 // This Wrapper is necessary
 SYSCALL_DEFINE1(readAddr, void*, p) {
-	long intital = 1;
+  long intital = 1;
 	
-	struct task_struct *task = current;
-	struct mm_struct *mm;
+  struct task_struct *task = current;
+  struct mm_struct *mm;
   struct vm_area_struct *mmap;
 
   task = get_current();	
   mm = task -> mm;
-	
-	// Use find vma instead of cycling through the vma linked list
-	mmap = find_vma(mm, (unsigned long)p);
+
+  // Use find vma instead of cycling through the vma linked list
+  mmap = find_vma(mm, (unsigned long)p);
   
-	if (mmap == NULL) {
+  if (mmap == NULL) {
     printk("\nFind VMA returned NULL\n");
-		return 0;
-	}
+    return 0;
+  }
 	
-	if( p >= mmap -> vm_end ) {
+  if( p >= mmap -> vm_end ) {
     printk ("\nPointer at end of VMA\n");
     return 0;
-	}
+  }
   
   printk("\nStarts at 0x%lx, Ends at 0x%lx\n", mmap -> vm_start, mmap -> vm_end);
-	return ( ( (mmap -> vm_flags) << 1 ) | intital);
+  return ( ( (mmap -> vm_flags) << 1 ) | intital);
 
 	
 }
